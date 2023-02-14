0d7b9077838ad03b5e23c74fd6837e360338d5c8
Yusuf <yusufali.5094@gmail.com>
2023-02-14 22:02:38 +0530

    quiz page

--- public/app.js
+++ public/app.js
@@ -1,1 +1,54 @@
-console.log("Linked");
+
+
+const radios = document.getElementsByClassName("radio")
+const submit = document.getElementById("submit-btn")
+const selectedQuizInput = document.getElementById("currentQuiz")
+// const startBtn = document.getElementById("quiz-btn-home")
+
+
+submit.addEventListener('click', (e)=>{
+
+    e.preventDefault()
+
+    let answers = []
+    let quiz = JSON.parse(selectedQuizInput.value)
+
+    for(let i=0; i<radios.length; i++){
+
+        if(radios[i].checked){
+
+            const checkedLabel = radios[i].labels[0].innerHTML
+            const checkedId = radios[i].name
+            answers.push({checkedLabel, checkedId})
+            
+        }
+        
+    }
+    
+    // console.log(answers);
+    compareAnswers(answers, quiz)
+})
+
+
+
+function compareAnswers(answers, quiz){
+
+    let crctCount = 0
+
+    for(let i=0; i<answers.length; i++){
+
+        if(answers[i].checkedId === quiz.questions[i].id){
+
+            if(quiz.questions[i].options[quiz.questions[i].crctOpIndex] === answers[i].checkedLabel){
+
+                crctCount++
+
+            }
+
+        }
+    }
+
+    console.log(crctCount);
+
+}
+
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt')
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path");
+const uniqid = require("uniqid")
 
 
 app.use(express.urlencoded({extended: false}))
@@ -98,11 +99,328 @@ app.get("/", (req, res)=>{
 app.get("/quizzes/:quizId", (req, res)=>{
 
     Quiz.findById(req.params.quizId, (err, result)=>{
-        res.render("quiz", {questions: result.questions})
+        res.render("quiz", {quiz: result})
     })
 
 })
 
+
+
+
+// API for sending questions for specific quiz by using quiz id
+
+app.get("/api/questions/:quizId", (req, res)=>{
+
+    Quiz.findById(req.params.quizId, (err, result)=>{
+        res.json(result.questions)
+    })
+
+})
+
+
+                                        /*************** API's For Results ************/
+
+
+
+
+
+// receiving and storing student results in database in results collection
+
+app.post("/api/result", (req, res)=>{
+
+    const body = req.body
+    
+    const result = new Result({
+        quizId: body.quizId,
+        userId: body.userId,
+        studentName: body.studentName,
+        correct: body.correct,
+        incorrect: body.incorrect,
+        totalMarks: body.totalMarks
+    })
+
+    result.save()
+    res.json({message: 'Results are successfully stored'})
+})
+
+
+
+
+
+// sending result of a specific quiz by using quiz id
+
+app.get("/api/results/:quizId", (req, res)=>{
+
+    Result.find({quizId: req.params.quizId}, (err, result)=>{
+        res.json(result);
+    })
+
+})
+
+
+                                            /*************** API's For Admin ************/
+
+
+
+// receiving data and creating a new quiz
+
+app.post("/api/admin/quiz", (req, res)=>{
+
+    const body = req.body
+    const quiz = new Quiz({
+
+        category: body.category,
+        questions: body.questionsArray
+
+    })
+
+    quiz.save().then((savedQuiz)=>{
+        res.send({ message: 'Quiz created successfully', quizId: quiz._id})
+    })
+
+})
+
+
+
+
+// update an entire quiz by using the quiz id
+
+app.put("/api/admin/quizzes/:id", (req, res)=>{
+
+    Quiz.findOneAndReplace({_id: req.params.id}, req.body, {upsert: true}, (err, doc)=>{
+
+        if(err){
+            res.send({err: err})
+        }
+        else{
+            res.send({message: "quiz updated successfully"})
+        }
+
+    })
+
+})
+
+
+
+
+
+// deleting a specific quiz by using its id
+
+app.delete("/api/admin/quizzes/:id", (req, res)=>{
+
+    Quiz.findByIdAndRemove(req.params.id, (err, deletedDoc)=>{
+
+        if(err){
+            res.send({err: err})
+        }
+        else{
+            res.send({message: "Deleted successfully"})
+        }
+
+    })
+
+})
+
+
+
+// Adding new question to the questions array in the database
+
+app.post("/api/admin/questions/:quizId", (req, res)=>{
+
+    Quiz.findOneAndUpdate({_id: req.params.quizId },  { $push: { questions: req.body } }, (err)=>{
+
+        if(err){
+            res.send({err: err})
+        } else{
+            res.send({message: "Successfully added the question"})
+        }
+
+    })
+
+})
+
+
+
+
+// Updating a specific question by using quizId and question id 
+
+app.put("/api/admin/questions/:quizId/:qid", (req, res)=>{
+
+    Quiz.findOneAndUpdate(
+        {_id: req.params.quizId,'questions.qid': parseInt(req.params.qid)}, 
+        {$set: {
+            'questions.$.ques': 'this question is fucked',
+            'questions.$.options': ['fuck1', 'fuck2', 'fuck3', 'fuck4'],
+            'questions.$.crctOpIndex': 0
+        }} ,
+        (err, result)=>{
+            
+            if(err){
+                res.send({err: err})
+            } else{
+                res.send({message: 'Question updated successfully'})
+            }
+        })
+
+})
+
+
+
+
+// deleting a specific question
+
+app.delete("/api/admin/questions/:quizId/:qid", (req, res)=>{
+
+    Quiz.findOneAndUpdate({_id: req.params.quizId }, { $pull: {'questions': {'qid': parseInt(req.params.qid) } } }, {safe: true, multi:true}, (err, result)=>{
+
+        if(err){
+            res.send({err: err})
+        } else{
+            res.send({message: 'Question deleted successfully'})
+        }
+
+    } )
+
+})
+
+
+                                        /*************** API's For Instructor ************/
+
+
+// sending all the results of all quizzes
+
+app.get("/api/instructor/results", (req, res)=>{
+
+    Result.find({}, (err, result)=>{
+        res.json(result)
+    })
+
+})
+
+
+
+
+
+
+// sending results of specific quiz
+
+app.get("/api/instructor/results/:quizId", (req, res)=>{
+
+    Result.find({ quizId: req.params.quizId }, (err, result)=>{
+        if(err){
+            res.send({err: err})
+        } else {
+            res.json(result)
+        }
+    })
+
+})
+
+
+
+
+                                        /*************** API's For Login And Registration ************/
+
+
+// creating a user
+
+app.post('/api/user', async (req, res)=>{
+
+    const body = req.body
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+    User.findOne({email: body.email}, (err, result)=>{
+
+        if(err){
+            res.json({err: 'something went wrong, Please try again...'})
+        }
+        if(result){
+            res.json({err: 'Account already exists with the given email.Please login'})
+        }
+        else{
+
+            const user = new User({
+                username: body.username,
+                email: body.email,
+                password: hashedPassword,
+                role: body.role
+            })
+        
+            user.save().then((savedUser)=>{
+        
+                res.json({message: 'Registered Successfully', userId: savedUser._id})
+        
+            }).catch((err)=>{
+        
+                res.json({err: err})
+                
+            })
+
+        }
+
+    })
+
+    
+
+})
+
+// logging in existing user
+
+app.post("/api/users/login", (req, res)=>{
+
+    const body = req.body;
+
+    User.findOne({ email: body.email }, (err, result)=>{
+
+        if(!result){
+
+            res.send({ authenticated: false, err: 'Account does not exists' })
+
+        }
+
+        if(result){
+
+            bcrypt.compare(body.password, result.password).then((match)=>{
+
+                if(match){
+                    res.send({authenticated: true, user: result.username, email: result.email, userId: result._id})
+                }
+                else{
+                    res.send({authenticated: false, err: 'Username or password is incorrect'})
+                }
+    
+            })
+
+        }
+        
+
+    })
+
+})
+
+
+
+// Delete a specific user by user id
+
+app.delete('/api/users/:userId', (req, res)=>{
+
+    body = req.body;
+    
+    User.findOneAndDelete({_id: req.params.userId}, (err, result)=>{
+
+        if(result){
+            res.json({message: 'Deleted successfully'})
+        } 
+        else {
+            res.json({err: 'User does not exist'})
+        }
+        if(err){
+            res.json({err: err})
+        }
+
+    })
+
+})
 
 
 
