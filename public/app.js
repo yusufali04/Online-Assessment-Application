2e4740d32222aede979fd664b2c9f46bfddfecd2
Yusuf <yusufali.5094@gmail.com>
2023-02-27 09:53:38 +0530

    completing quiz page

--- public/app.js
+++ public/app.js
@@ -1,14 +1,20 @@
 
-
 const radios = document.getElementsByClassName("radio")
 const submit = document.getElementById("submit-btn")
 const selectedQuizInput = document.getElementById("currentQuiz")
-// const startBtn = document.getElementById("quiz-btn-home")
+// submited page
+const expirePage = document.getElementById('timeOutWarningOverlay')
+const quizWrapper = document.getElementById("quiz-wrapper")
 
 
-submit.addEventListener('click', (e)=>{
+submit.addEventListener('click', submitQuiz)
 
-    e.preventDefault()
+function submitQuiz(e){
+
+
+    if(e){
+        e.preventDefault()
+    }
 
     let answers = []
     let quiz = JSON.parse(selectedQuizInput.value)
@@ -25,30 +31,117 @@ submit.addEventListener('click', (e)=>{
         
     }
     
+
     // console.log(answers);
+    clearInterval(intervalId);
     compareAnswers(answers, quiz)
-})
+    expirePage.style.visibility="visible"
+    quizWrapper.style.visibility="hidden"
+
+}
 
 
 
 function compareAnswers(answers, quiz){
 
+    console.log(answers);
     let crctCount = 0
+    let incorrectCount = 0
 
-    for(let i=0; i<answers.length; i++){
 
-        if(answers[i].checkedId === quiz.questions[i].id){
+    for(let i=0; i< quiz.questions.length; i++){
 
-            if(quiz.questions[i].options[quiz.questions[i].crctOpIndex] === answers[i].checkedLabel){
+        for(let j=0; j< answers.length; j++){
 
-                crctCount++
-
+            if(answers[j].checkedId == quiz.questions[i].qid){
+    
+                if(quiz.questions[i].options[quiz.questions[i].crctOpIndex] == answers[j].checkedLabel){
+    
+                    crctCount++
+    
+                }
+                else{
+                    incorrectCount++
+                }
+    
             }
 
         }
+
+        
     }
 
-    console.log(crctCount);
+    const userId = document.getElementById("userId")
+    const quizId = document.getElementById("quizId")
+    const studentName = document.getElementById("studentName")
+    const correct = crctCount
+    const incorrect = incorrectCount
+    const unAttempted = quiz.questions.length - (crctCount + incorrectCount)
+    const totalMarks = crctCount
+
+    const resultData = {
+        userId: userId.value,
+        quizId: quizId.value,
+        studentName: studentName.value,
+        correct,
+        incorrect,
+        unAttempted,
+        totalMarks
+    }
+
+    axios.post("/api/result", resultData).then((res)=>{
+        console.log(res);
+    })
 
 }
+
+// if (window.location.href.indexOf("http://localhost:3000/quizzes/") === 0) {
+//   // your JavaScript code here
+//   console.log("This code will only execute on quiz pages.");
+// }
+
+const counterElement = document.getElementById("counter");
+
+const durationInSeconds = 60;
+let remainingSeconds = durationInSeconds;
+
+let intervalId = setInterval(callback, 1000);
+
+
+function callback(){
+
+    const timeformat = time_convert(remainingSeconds)
+    counterElement.innerHTML = timeformat
+    
+    remainingSeconds--;
+
+    if(remainingSeconds < 30){
+        counterElement.style.color='red'
+    }
+
+    if (remainingSeconds < 0) {
+
+        clearInterval(intervalId);
+        submitQuiz()
+
+    }
+
+}
+
+function time_convert(num)
+ { 
+
+  let minutes = Math.floor(num / 60);  
+  let seconds = num % 60;
+
+  if(minutes.toString().length==1){
+    minutes = '0'+minutes
+  }
+  if(seconds.toString().length==1){
+    seconds = '0'+seconds
+  }
+
+  return minutes + ":" + seconds;         
+}
+
 
--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const bcrypt = require('bcrypt')
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 const path = require("path");
-const uniqid = require("uniqid")
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require("express-session");
+const flash = require('express-flash');
+const MongoStore = require('connect-mongo');
 
 
 app.use(express.urlencoded({extended: false}))
@@ -22,6 +26,47 @@ app.use(cors())  // Just an API configuration
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
+
+
+
+/*************************** Session store in database ********************************/
+
+let sessionStore = MongoStore.create({
+    mongoUrl: 'mongodb://localhost:27017/quiz',
+    collection: 'sessions'
+});
+
+// creating cookie for passport
+
+app.use(session({
+    secret: 'thisIsSecret',
+    resave: false,
+    store: sessionStore,
+    saveUninitialized: false,
+    // cookie: {expires: 30000}
+}));
+
+app.use(flash());
+
+/************** passport config ******************/
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// global middleware
+
+app.use((req,res,next)=>{
+
+    res.locals.session = req.session;
+    res.locals.user = req.user;
+    next();
+
+})
+
+
+
 
 
 
@@ -45,6 +90,9 @@ connection.once('open',(err) => {
 
 const Schema = mongoose.Schema;
 
+
+
+
 /******** creating quizzes collection *****/
 
 const quizSchema = new Schema({
@@ -64,6 +112,7 @@ const resultsSchema = new Schema({
     studentName: String,
     correct: Number,
     incorrect: Number,
+    unAttempted: Number,
     totalMarks: Number
 })
 
@@ -86,7 +135,15 @@ const User = mongoose.model('user', usersSchema);
 
 
 
-// Routes
+// GET Routes
+
+app.get("/register", (req, res)=>{
+    res.render("register")
+})
+
+app.get("/login", (req, res)=>{
+    res.render("login")
+})
 
 app.get("/", (req, res)=>{
 
@@ -98,24 +155,79 @@ app.get("/", (req, res)=>{
 
 app.get("/quizzes/:quizId", (req, res)=>{
 
+    if(!req.isAuthenticated()){
+        return res.redirect("/login")
+    }
+
     Quiz.findById(req.params.quizId, (err, result)=>{
         res.render("quiz", {quiz: result})
     })
 
 })
 
+//for results page
 
+app.get("/instructor/results/:quizId", (req, res)=>{
 
-
-// API for sending questions for specific quiz by using quiz id
-
-app.get("/api/questions/:quizId", (req, res)=>{
-
-    Quiz.findById(req.params.quizId, (err, result)=>{
-        res.json(result.questions)
-    })
+    
 
 })
+
+
+
+// POST Routes
+
+// creating a user
+
+app.post('/api/user', async (req, res)=>{
+
+    const body = req.body
+    const hashedPassword = await bcrypt.hash(body.password, 10)
+
+    User.findOne({email: body.email}, (err, result)=>{
+
+        if(err){
+
+            req.flash('regError', 'Something went wrong.Try again')
+            res.redirect('/register')
+
+        }
+        if(result){
+
+            req.flash('regError', 'Account already exists')
+            res.redirect('/register')
+
+        }
+        else{
+
+            const user = new User({
+                username: body.username,
+                email: body.email,
+                password: hashedPassword,
+                role: body.role
+            })
+        
+            user.save().then((savedUser)=>{
+        
+                res.redirect("/login")
+        
+            }).catch((err)=>{
+        
+                res.json({err: err})
+                
+            })
+
+        }
+
+    })
+
+    
+
+})
+
+
+
+
 
 
                                         /*************** API's For Results ************/
@@ -136,11 +248,20 @@ app.post("/api/result", (req, res)=>{
         studentName: body.studentName,
         correct: body.correct,
         incorrect: body.incorrect,
+        unAttempted: body.unAttempted,
         totalMarks: body.totalMarks
     })
 
-    result.save()
-    res.json({message: 'Results are successfully stored'})
+    result.save().then((savedResult)=>{
+
+        if(!savedResult){
+            res.send("Something went wrong")
+        }
+        else{
+            res.send("Stored successfully")
+        }
+    })
+
 })
 
 
@@ -322,51 +443,11 @@ app.get("/api/instructor/results/:quizId", (req, res)=>{
                                         /*************** API's For Login And Registration ************/
 
 
-// creating a user
 
-app.post('/api/user', async (req, res)=>{
-
-    const body = req.body
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-
-    User.findOne({email: body.email}, (err, result)=>{
-
-        if(err){
-            res.json({err: 'something went wrong, Please try again...'})
-        }
-        if(result){
-            res.json({err: 'Account already exists with the given email.Please login'})
-        }
-        else{
-
-            const user = new User({
-                username: body.username,
-                email: body.email,
-                password: hashedPassword,
-                role: body.role
-            })
-        
-            user.save().then((savedUser)=>{
-        
-                res.json({message: 'Registered Successfully', userId: savedUser._id})
-        
-            }).catch((err)=>{
-        
-                res.json({err: err})
-                
-            })
-
-        }
-
-    })
-
-    
-
-})
 
 // logging in existing user
 
-app.post("/api/users/login", (req, res)=>{
+app.post("/api/", (req, res)=>{
 
     const body = req.body;
 
@@ -383,7 +464,7 @@ app.post("/api/users/login", (req, res)=>{
             bcrypt.compare(body.password, result.password).then((match)=>{
 
                 if(match){
-                    res.send({authenticated: true, user: result.username, email: result.email, userId: result._id})
+                    res.send({authenticated: true, username: result.username, email: result.email, userId: result._id, role: result.role})
                 }
                 else{
                     res.send({authenticated: false, err: 'Username or password is incorrect'})
@@ -421,6 +502,107 @@ app.delete('/api/users/:userId', (req, res)=>{
     })
 
 })
+
+
+
+
+
+// Passport configuration
+
+
+passport.use(new LocalStrategy({usernameField: 'email'}, async (email, password, done) => {
+
+    const user = await User.findOne({email:email});
+
+    if(!user){
+
+        return done(null, false, {message: 'No user found with this email'});
+
+    } else {
+        bcrypt.compare(password, user.password).then(match => {
+
+            if(match){
+
+                done(null, user, {message: 'logged in successfully'});
+
+            } else {
+
+                done(null, false, {message: 'Invalid username or password'});
+
+            }
+        }).catch(err => {
+
+            return done(null, false, {message: 'Something went wrong'});
+
+        })
+    }
+
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    })
+})
+
+
+
+
+app.post("/api/users/login", (req, res, next)=>{
+
+    passport.authenticate('local', (err, user, info) => {
+        if(err){
+
+            res.flash('error', info.message);
+            res.redirect('/login')
+            
+        }
+        if(!user){
+
+            req.flash('error', info.message);
+            res.redirect('/login')
+
+        } else {
+
+            req.logIn(user, (err) => {
+
+                if(err){
+                    req.flash('error', info.message);
+                    return next(err);
+                } else {
+                    return res.redirect('/')
+                }
+                
+            })
+        }
+
+
+})(req, res, next)
+
+})
+
+
+app.get("/logout", (req, res)=>{
+
+    req.logout((err)=>{
+
+        if(!err){
+            res.redirect("/login")
+        }
+
+    })
+
+})
+
+
+
+
+
+
 
 
 
